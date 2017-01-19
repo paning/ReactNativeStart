918d261498a32f4a783810e6159c6f7b873a7e30
paning <pwcn_78@163.com>
2017-01-19 13:22:13 +0800

    去掉环信联系人界面三个功能

--- android/app/src/main/java/com/kalix/android/IM/ui/ContactListFragment.java
+++ android/app/src/main/java/com/kalix/android/IM/ui/ContactListFragment.java
@@ -188,18 +188,18 @@ public class ContactListFragment extends EaseContactListFragment {
                 // 进入申请与通知页面
                 startActivity(new Intent(getActivity(), NewFriendsMsgActivity.class));
                 break;
-            case R.id.group_item:
-                // 进入群聊列表页面
-                startActivity(new Intent(getActivity(), GroupsActivity.class));
-                break;
-            case R.id.chat_room_item:
-                //进入聊天室列表页面
-                startActivity(new Intent(getActivity(), PublicChatRoomsActivity.class));
-                break;
-            case R.id.robot_item:
-                //进入Robot列表页面
-//                startActivity(new Intent(getActivity(), RobotsActivity.class));
-                break;
+//            case R.id.group_item:
+//                // 进入群聊列表页面
+//                startActivity(new Intent(getActivity(), GroupsActivity.class));
+//                break;
+//            case R.id.chat_room_item:
+//                //进入聊天室列表页面
+//                startActivity(new Intent(getActivity(), PublicChatRoomsActivity.class));
+//                break;
+//            case R.id.robot_item:
+//                //进入Robot列表页面
+////                startActivity(new Intent(getActivity(), RobotsActivity.class));
+//                break;
 
             default:
                 break;
